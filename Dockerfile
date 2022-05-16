5de92e06a82c48421456abeda68f3d0c8704290c
Maximilien Fornette <MaxF@MaX.local>
2022-05-16 13:34:34 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM python
+
+CMD ["echo", "Hello World!!"]
+
---
+++ oc_lettings_site/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import index
+import lettings.views
+import profiles.views
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('lettings/', lettings.views.index, name='lettings_index'),
+    path('lettings/<int:letting_id>/', lettings.views.letting, name='letting'),
+    path('profiles/', profiles.views.index, name='profiles_index'),
+    path('profiles/<str:username>/', profiles.views.profile, name='profile'),
+    path('admin/', admin.site.urls),
+]
